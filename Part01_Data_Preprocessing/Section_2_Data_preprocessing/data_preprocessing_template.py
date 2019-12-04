fc4fac6b738eb38af99307bd1eedcda38f6ab86d
JavierDominguezGomez <jdomgo3@gmail.com>
2019-12-04 21:25:10 +0100

    Data preprocessing

---
+++ Part01_Data_Preprocessing/Section_2_Data_preprocessing/data_preprocessing_template.py
@@ -0,0 +1,10 @@
+# Plantilla de preprocesado
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importar el data set
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values

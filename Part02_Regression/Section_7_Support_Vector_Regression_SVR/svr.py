b312e258db8c20512a42593ed83999e672fe7801
JavierDominguezGomez <jdomgo3@gmail.com>
2019-12-20 10:59:45 +0100

    Regresión con Máquinas de Soporte Vectorial (SVR).

--- Part02_Regression/Section_7_Support_Vector_Regression_SVR/svr.py
+++ Part02_Regression/Section_7_Support_Vector_Regression_SVR/svr.py
@@ -1,11 +1,11 @@
-# Plantilla de regresión
+# SVR
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importar el dataset
-dataset = pd.read_csv('xxxxxxxxx.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -17,26 +17,27 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 '''
 
 # Escalado de variables
-'''from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)'''
+sc_y = StandardScaler()
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y.reshape(-1, 1))
 
 # Ajustar la regresión con el dataset
-# Crear aquí nuestro modelo de regresión
-regression = None
+from sklearn.svm import SVR
+regression = SVR(kernel = 'rbf')
+regression.fit(x, y)
 
 
-# Predicción de nuestros modelos
-y_pred = regression.predict([[6.5]])
+# Predicción de nuestros modelos con SVR
+y_pred = sc_y.inverse_transform(regression.predict(sc_x.transform([[6.5]])))
 
-
-# Visualización de los resultados del modelo
+# Visualización de los resultados del SVR
 x_grid = np.arange(min(x), max(x), 0.1)
 x_grid = x_grid.reshape(len(x_grid), 1)
 plt.scatter(x, y, color = 'red')
 plt.plot(x_grid, regression.predict(x_grid), color = 'blue')
-plt.title('Modelo de regresión')
+plt.title('Modelo de regresión (SVR)')
 plt.xlabel('Posición del empleado')
 plt.ylabel('Sueldo (en $)')
 plt.show()

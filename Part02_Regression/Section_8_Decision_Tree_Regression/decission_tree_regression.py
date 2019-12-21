0dbffef2d459cf076e76039f59f8ae89984ee11e
JavierDominguezGomez <jdomgo3@gmail.com>
2019-12-21 21:19:36 +0100

    Regresión con bosques aleatorios.

---
+++ Part02_Regression/Section_8_Decision_Tree_Regression/decission_tree_regression.py
@@ -0,0 +1,41 @@
+# Regresión con árboles de decisión
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importar el dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+
+# Dividir el dataset en conjunto de entrenamiento y conjunto de testing
+'''
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+'''
+
+# Escalado de variables
+'''from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)'''
+
+# Ajustar la regresión con el dataset
+from sklearn.tree import DecisionTreeRegressor
+regression = DecisionTreeRegressor(random_state = 0)
+regression.fit(x, y)
+
+# Predicción de nuestros modelos
+y_pred = regression.predict([[6.5]])
+
+# Visualización de los resultados del modelo
+#x_grid = np.arange(min(x), max(x), 0.1)
+#x_grid = x_grid.reshape(len(x_grid), 1)
+plt.scatter(x, y, color = 'red')
+plt.plot(x, regression.predict(x), color = 'blue')
+plt.title('Modelo de regresión')
+plt.xlabel('Posición del empleado')
+plt.ylabel('Sueldo (en $)')
+plt.show()
